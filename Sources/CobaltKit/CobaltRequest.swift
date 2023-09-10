62e44faa73781291d3bace73daac4d024ac781e2
paytondev <81662879+paytontech@users.noreply.github.com>
2023-09-10 11:15:01 -0400

    please just work :(

--- Sources/CobaltKit/CobaltRequest.swift
+++ Sources/CobaltKit/CobaltRequest.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 public class CobaltRequest: Codable {
+    init(url: String) {
+        //default settings
+        self.url = url
+    }
     init(url: String, vCodec: String, vQuality: Int, aFormat: String, isAudioOnly: Bool, isNoTTWatermark: Bool, isTTFullAudio: Bool, isAudioMuted: Bool, dubLang: Bool) {
         self.url = url
         self.vCodec = vCodec
